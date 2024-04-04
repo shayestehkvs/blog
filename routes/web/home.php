8c3ad7a5f1148bbfb7d3da021d380a957aab9ddb
Shayesteh kavoosi <shayestehkvs@gmail.com>
2024-04-04 16:44:48 +0330

    add users

--- routes/web/home.php
+++ routes/web/home.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,9 +36,18 @@ Route::middleware('auth')->group(function () {
     Route::get('all-categories', [CategoryController::class, 'allCategories'])->name('all-categories');
     Route::get('create-category', [CategoryController::class, 'createCategory'])->name('create-category');
     Route::post('store-category', [CategoryController::class, 'storeCategory'])->name('store-category');
-    Route::get('create-edit/{id}', [CategoryController::class, 'editCategory'])->name('edit-category');
+    Route::get('category-edit/{id}', [CategoryController::class, 'editCategory'])->name('edit-category');
     Route::put('category-update/{id}', [CategoryController::class, 'updateCategory'])->name('update-category');
     Route::delete('/delete-category/{id}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
+
+    //user management
+    Route::get('/all-users', [UserController::class, 'allUser'])->name('all-users');
+    Route::get('create-user', [UserController::class, 'createUser'])->name('create-user');
+    Route::post('store-user', [UserController::class, 'storeUser'])->name('store-user');
+    Route::get('user-edit/{id}', [UserController::class, 'editUser'])->name('edit-user');
+    Route::put('user-update/{id}', [UserController::class, 'updateUser'])->name('update-user');
+    Route::delete('/delete-user/{id}', [UserController::class, 'deleteUser'])->name('delete-user');
+
 });
 
 require __DIR__.'/auth.php';
